3c729cb715a563b8d088f830695d3e2d1e6f1e65
Vasconcelos <j.a.vasconcelos@accenture.com>
2019-11-17 18:02:06 -0300

    deploy no heroku

--- src/app/services/doemaisapi.ts
+++ src/app/services/doemaisapi.ts
@@ -1,2 +1,6 @@
 //constante responsável por apontar o API.
-export const DOE_MAIS_API: string = 'http://localhost:8080';
+//export const DOE_MAIS_API: string = 'http://localhost:8080';
+export const DOE_MAIS_API: string = 'https://doemaisbackend.herokuapp.com';
+
+
+
